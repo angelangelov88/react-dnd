0c67a0e561f143841b3e1f7be02640045e1478c7
Angel <angel.levski@gmail.com>
2024-10-02 15:18:36 +0100

    Added storyblok
    Added it as a hook and installed it
    Then used it to fetch data and display the titles

---
+++ dnd-app/src/hooks/useStoryblok.js
@@ -0,0 +1,41 @@
+import {  useState, useMemo, useEffect,  } from 'react';
+import StoryblokClient from 'storyblok-js-client';
+
+
+export default () => {
+	const [stories, setStories] = useState(false);
+	const accessToken = process.env.REACT_APP_STORYBLOK_TOKEN;
+	console.log('Environment Variables:', process.env);
+
+	const getStories = () => {
+		const Storyblok = new StoryblokClient({
+			accessToken: accessToken,
+			region: 'eu',
+		});
+  
+		// 3. Retrieve stories
+		Storyblok.get('cdn/stories', {
+		// version: 'published',
+		})
+			.then((response) => {
+				console.log('Stories', response);
+				setStories(response?.data?.stories);
+			})
+			.catch((error) => {
+				console.error('Error', error);
+			});
+	};
+  
+	useEffect(() => {
+		getStories();
+	}, []);
+  
+	return useMemo(
+		() => ({
+			stories,
+		}),
+		[
+			stories,
+		],
+	);
+};
