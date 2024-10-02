4acabd6e0851b7e1eae0dd129b79b717c824ecaa
Angel <angel.levski@gmail.com>
2024-10-02 11:24:30 +0100

    Added a component

--- dnd-app/src/App.js
+++ dnd-app/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { BothContainer } from './BothContainer';
-import ResizableComp from './ResizableComp';
-import { Container } from './Container';
-import RncComp from './RncComp';
+// import { DndProvider } from 'react-dnd';
+// import { HTML5Backend } from 'react-dnd-html5-backend';
+// import { BothContainer } from './BothContainer';
+// import ResizableComp from './ResizableComp';
+// import { Container } from './Container';
+// import RncComp from './RncComp';
+// import WebkitResizer from './WebkitResizer';
 import './index.css';
-import WebkitResizer from './WebkitResizer';
+import StoryBlok from './StoryBlok';
 
 function App() {
 	return (
 		<div className="App">
-			<DndProvider backend={HTML5Backend}>
+			{/* <DndProvider backend={HTML5Backend}>
 				<Container />
 				<BothContainer />
 			</DndProvider>
 			<ResizableComp />
 			<RncComp />
-			<WebkitResizer />
+			<WebkitResizer /> */}
+			<StoryBlok />
 		</div>
 	);
 }
---
+++ dnd-app/src/StoryBlok.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StoryBlok = () => {
+	return ( 
+		<div>
+			<h1>StoryBlok</h1>
+			<p>more info</p>
+		</div>
+	);
+};
+ 
+export default StoryBlok;
