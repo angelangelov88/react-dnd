434c4d1940f6b76ff3b157a76ed641b45fa500ee
Angel <angel.levski@gmail.com>
2024-10-04 14:37:51 +0100

    Removed storyblok

--- dnd-app/src/App.js
+++ dnd-app/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
-// import { BothContainer } from './BothContainer';
-// import ResizableComp from './ResizableComp';
-// import { Container } from './Container';
-// import RncComp from './RncComp';
-// import WebkitResizer from './WebkitResizer';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BothContainer } from './BothContainer';
+import ResizableComp from './ResizableComp';
+import { Container } from './Container';
+import RncComp from './RncComp';
+import WebkitResizer from './WebkitResizer';
 import './index.css';
-import StoryBlok from './StoryBlok';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <DndProvider backend={HTML5Backend}>
+			<DndProvider backend={HTML5Backend}>
 				<Container />
 				<BothContainer />
 			</DndProvider>
 			<ResizableComp />
 			<RncComp />
-			<WebkitResizer /> */}
-			<StoryBlok />
+			<WebkitResizer />
 		</div>
 	);
 }
